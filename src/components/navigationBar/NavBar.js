32e495e55be9068c0ff81cedf262f38886b35c7c
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-07 10:55:01 -0500

    nav bar

--- src/components/navigationBar/NavBar.js
+++ src/components/navigationBar/NavBar.js
@@ -4,11 +4,15 @@ function NavBar() {
     return (
         <nav className="nav">
             <a href="/" className="site-title">Anime Bar</a>
-            <a className="top-nav" href="Top">Top</a>
-            <a className="about-nav" href="/about">About</a>
+            <ul>
+                <li className="active">
+                    <a href="Top">Top</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
         </nav>
-
-        
     )
 }
 
