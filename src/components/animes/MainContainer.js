71a31a9ec99928457ab1d5e3c47b84073f1b34c7
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-07 19:07:16 -0500

    sandy

--- src/components/animes/MainContainer.js
+++ src/components/animes/MainContainer.js
@@ -1,28 +1,16 @@
 import React from 'react'
 import AnimeContainer from "./AnimeContainer"
-<<<<<<< HEAD:src/components/displayAnime/MainContainer.js
-import TopRatedContainer from './TopRatedContainer'
-import RecommendedAnimes from './RecommendedAnimes'
+
+
 //import PopUpDescription from '../animeDescription/PopUpModal'
-=======
 import TopRatedContainer from '../topRatedAnime/TopRatedContainer'
->>>>>>> main:src/components/animes/MainContainer.js
+
 
 function MainContainer({animeList, recommendedAnimesApi, setRecommendedAnimes, recommendedAnimes}) {
 
     return (
         <div className="main-container">
-            {/* <RecommendedAnimes 
-                recommendedAnimes={recommendedAnimes} 
-                animeList={animeList}
-<<<<<<< HEAD:src/components/displayAnime/MainContainer.js
-                // setImgClicked={setImgClicked}
-                // imgClicked={imgClicked}
-            />
-            <TopRatedContainer animeList={animeList}/>
-=======
-            /> */}
->>>>>>> main:src/components/animes/MainContainer.js
+           
             <AnimeContainer animeList={animeList}/>
             <TopRatedContainer animeList={animeList}/>
 
